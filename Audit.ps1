cbc22835e04a95b072053b31b17983803d5c8d51
Kisuma <qperon@outlook.com>
2018-05-24 10:18:32 +0200

    Script d'audit de pc

---
+++ Audit.ps1
@@ -0,0 +1,20 @@
+﻿Set-Location Env:
+
+Get-WmiObject Win32_LogicalDisk | ft
+$table = "COMPUTERNAME","OS","NUMBER_OF_PROCESSORS","USERNAME","SystemRoot"
+
+foreach($item in $table){
+
+    Get-ChildItem $item
+
+}
+Get-CimInstance -ClassName Win32_ComputerSystem | fl
+
+Get-CimInstance -ClassName Win32_QuickFixEngineering | Select HotFixID,Description | ft
+
+$os = Get-WmiObject -Class win32_operatingsystem
+$os.ConvertToDateTime($os.LastBootUpTime)
+
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | ft
+
+Get-CimInstance -ClassName Win32_Service | Select-Object -Property State,Name,DisplayName
---
+++ Spinner.ps1
@@ -0,0 +1,31 @@
+﻿[String]$str = "loading"
+[String]$strUP = $str.ToUpper()
+while($true){
+    for($cpt=0;$cpt -lt 4;$cpt++){
+    
+        switch($cpt){
+            0 { $spin = "|"
+                $dots = ""}
+            1 { $spin = "/"
+                $dots = "."}
+            2 { $spin = "-"
+                $dots = ".."}
+            3 { $spin = "\"
+                $dots = "..."}   
+        }
+
+        
+
+        Write-Host "[$spin] $dots"
+        sleep -Milliseconds 75
+        cls
+
+    }
+
+    <#for($i=0;$i -lt $str.Length;$i++){
+
+        echo $str.Replace($str[$i],$strUP[$i])
+        sleep -Milliseconds 100
+        
+    }#>
+}    
